3755a008080b8d2d36aaa1fab909e64ace5be54b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-02-20 12:45:56 +1300

    Added walking animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,8 +6,28 @@ var speed = 400  # speed in pixels/sec
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-
 	move_and_slide()
+
+func _input(event):
+		##If left or right is pressed, it plays the relevant walk animation
+	if Input.is_action_just_pressed("left"):
+		$AnimatedSprite2D.play("Walking side")
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_just_pressed("right"):
+		$AnimatedSprite2D.play("Walking side")
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_just_pressed("up"):
+		$AnimatedSprite2D.play("Walking up")
+	elif Input.is_action_just_pressed("down"):
+		$AnimatedSprite2D.play("Walking dow")
+
+		##Plays the idle animation once the left or right button is released
+	if Input.is_action_just_released("left") or Input.is_action_just_released("right"):
+		$AnimatedSprite2D.play("Idle side")
+	elif Input.is_action_just_released("up"):
+		$AnimatedSprite2D.play("Idle up")
+	elif Input.is_action_just_released("down"):
+		$AnimatedSprite2D.play("Idle down")
 	
 func _ready() -> void:
 	self.position.x = Globals.player_x

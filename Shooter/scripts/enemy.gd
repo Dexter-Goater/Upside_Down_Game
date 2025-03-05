b033b43d521267ca060261a4a63c643e10a461a2
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-03-06 12:46:57 +1300

    added shooting and raycasts

--- Shooter/scripts/enemy.gd
+++ Shooter/scripts/enemy.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 class_name Enemy
 
 var player :Player = null
-
+var hit_points:int = 3
 var speed: float = 100.0
 var direction := Vector2.ZERO
+var stop_distance := 20.0
 func _process(delta):
 	if player != null:
 		look_at(player.global_position)
@@ -12,7 +13,11 @@ func _process(delta):
 func _physics_process(delta):
 	if player != null:
 		var enemy_to_player = (player.global_position - global_position)
-		direction = enemy_to_player.normalized()
+		if enemy_to_player.length() > stop_distance:
+			direction = enemy_to_player.normalized()
+		else:
+			direction = Vector2.ZERO
+		
 		if direction != Vector2.ZERO:
 			velocity = speed * direction
 		else:
@@ -34,3 +39,12 @@ func _on_player_detector_body_exited(body):
 		if player != null:
 			player = null
 			print(name + "lost the player")
+
+func take_damage(amount: int):
+	if amount > 0:
+		hit_points -= amount
+		#play enemy hurt sounds
+		if hit_points <= 0:
+			print("enemy dies")
+			queue_free()
+	
--- Shooter/scripts/player_shooter.gd
+++ Shooter/scripts/player_shooter.gd
@@ -6,8 +6,10 @@ signal died
 
 
 @onready var camera_remote_transform = $CameraRemoteTransform
-
+@onready var shoot_raycast = $ShootRaycast
+@onready var anim = $AnimatedSprite2D
 var speed = 400.0
+var canshoot = true
 
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
@@ -20,7 +22,18 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, 0.0, 0.2)
 		velocity.y = lerp(velocity.y, 0.0, 0.2)
-	
+
+	if Input.is_action_just_pressed("shoot"):
+		if canshoot == true:
+			anim.play("Shooting")
+			$Timer.start()
+		if shoot_raycast.is_colliding():
+			var collider = shoot_raycast.get_collider()
+			if collider is StaticBody2D:
+				print("shot box")
+			elif collider is Enemy:
+				collider.player = self
+				collider.take_damage(1)
 	
 	move_and_slide()
 
@@ -29,3 +42,7 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body is Enemy:
 		died.emit()
 		queue_free()
+
+
+func _on_timer_timeout():
+	var canshoot = true
